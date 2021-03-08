ff7d364664b60422f38d965ed1c50d691b64481e
Muhammad Luthfi Ichsanudin <67815792+MuhammadLuthfiI04@users.noreply.github.com>
2021-03-08 15:23:11 +0700

    Update matematika.java

--- Pertemuan2/Teori/matematika.java
+++ Pertemuan2/Teori/matematika.java
@@ -1,22 +1,25 @@
 public class matematika {
     int hasil = 0;
     int bilangan1 = 0;
-
+    
+    // Rumus Pertambahan 
     void pertambahan(int newBilangan){
         hasil =  bilangan1 + newBilangan;
         System.out.println("\nPertambahan: "+ bilangan1 + " + " + newBilangan + " = " + hasil);
     }
     
+    // Rumus Pengurangan 
     void pengurangan(int newBilangan){
         hasil =  bilangan1 - newBilangan;
         System.out.println("\nPengurangan: "+ bilangan1 + " - " + newBilangan + " = " + hasil);
     }
     
+    // Rumus Perkalian 
     void perkalian(int newBilangan){
         hasil =  bilangan1 * newBilangan;
         System.out.println("\nPerkalian: "+ bilangan1 + " x " + newBilangan + " = " + hasil);
     }
-    
+    // Rumus Pembagian 
     void pembagian(int newBilangan){
         hasil =  bilangan1 / newBilangan;
         System.out.println("\nPembagian: "+ bilangan1 + " / " + newBilangan + " = " + hasil);
