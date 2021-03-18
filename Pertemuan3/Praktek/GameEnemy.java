2cf2e45dc7ab7a6cc8e2cae199f29b3a8922a5b8
Muhammad Luthfi Ichsanudin <67815792+MuhammadLuthfiI04@users.noreply.github.com>
2021-03-18 12:33:14 +0700

    Add files via upload

---
+++ Pertemuan3/Praktek/GameEnemy.java
@@ -0,0 +1,47 @@
+public class GameEnemy {
+    // ATTRIBUTE
+    double width;
+    double height;
+    int positionX;
+    int positionY;
+    
+    //METHOD
+    GameEnemy(){
+            
+    }
+        
+    GameEnemy(double width, double height){
+
+    }
+        
+    void setDimension(double width, double height){
+        this.width = width;
+        this.height = height;
+
+    }
+    
+    void setPosition(int positionX, int positionY){
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+    
+    double getWidth() {
+        return this.width;
+    }
+    
+    double getHeight() {
+        return this.height;
+    }
+    
+    int getPositionX() {
+        return this.positionX;
+    }
+    
+    int getPositionY() {
+        return this.positionY;
+    }
+    
+    void Run(){
+        System.out.println("Enemy is running");
+    }
+}
