b248a8698593f37bc0340efd86b649b1d2e38de7
Muhammad Luthfi Ichsanudin <67815792+MuhammadLuthfiI04@users.noreply.github.com>
2021-03-18 12:00:06 +0700

    Add files via upload

---
+++ Pertemuan3/Praktek/GamePlayer.java
@@ -0,0 +1,51 @@
+public class GamePlayer {
+    // ATTRIBUTE
+    double width;
+    double height;
+    int positionX;
+    int positionY;
+    
+    //METHOD
+    GamePlayer(){
+        
+    }
+    
+    GamePlayer(double width, double height, int positionX, int positionY){
+        
+    }
+    
+    void setDimension(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    void setPosition(int positionX, int positionY){
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    double getWidth() {
+        return this.width;
+    }
+
+    double getHeight() {
+        return this.height;
+    }
+
+    int getPositionX() {
+        return this.positionX;
+    }
+
+    int getPositionY() {
+        return this.positionY;
+    }
+
+    void Run(){
+        System.out.println("Player is running");
+    }
+
+    void Run(int incrementX ){
+        incrementX = incrementX + positionX;
+        System.out.println("Player still running...current X position = " + incrementX);
+    }
+}
