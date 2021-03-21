ba01bca105a632c3a143978d77fed6a90802ee3f
Muhammad Luthfi Ichsanudin <67815792+MuhammadLuthfiI04@users.noreply.github.com>
2021-03-21 12:23:01 +0700

    Add files via upload

---
+++ Pertemuan3/Praktek/TestGame.java
@@ -0,0 +1,7 @@
+public class TestGame {
+    public static void main(String[] args){
+        GamePlayer Demo = new GamePlayer();
+        Demo.setPosition(30,40);
+        Demo.Run(10);   
+     }
+}
